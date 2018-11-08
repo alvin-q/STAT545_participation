65089fccc8a969d87746e512c7b095912490f46c
Alvin Qiu <alvinj.qiu@hotmail.com>
2018-11-08 10:47:50 -0800

    finish cm 106

---
+++ STAT547/cm105/powers/R/data.R
@@ -0,0 +1,15 @@
+#' Integer vector from 1 to 10
+#'
+#' Self-explanatory!
+#'
+#' @format What format does you data take? Integer vector.
+#' @source Where did the data come from?
+"tenvec"
+
+#' Dataframe from 1 to 10
+#'
+#' Self-explanatory!
+#'
+#' @format What format does you data take? Integer vector.
+#' @source Where did the data come from?
+"tendf"
